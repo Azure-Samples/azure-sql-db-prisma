fa39c3d539bba4ec8ebf73ab4033fe05c1303082
Davide Mauri <damauri@microsoft.com>
2020-08-04 12:33:40 -0700

    completed rest api

--- api/todoUpdate/index.js
+++ api/todoUpdate/index.js
@@ -1,62 +1,13 @@
-require('dotenv').config();
+const { executeSQL } = require('../shared/utils');
 
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request
-var TYPES = require('tedious').TYPES;
+todoUpdate = function (context, req) {
+    var payload = { 
+        "id": req.params.id,
+        "todo": req.body
+    };
 
-getTodo = function (context, req) {
-    const toDo = req.body;
-    var result = "";    
-
-    var connection = new Connection({
-        server: process.env["db_server"],
-        authentication: {
-            type: 'default',
-            options: {
-                userName: process.env["db_user"],
-                password: process.env["db_password"],
-            }
-        },
-        options: {
-            database: process.env["db_database"],
-            encrypt: true
-        }
-    });
-
-    connection.connect();
-
-    connection.on('connect', err => {
-        if (err) {
-            context.log(err);
-            context.done();
-        }
-        else {
-            executeStatement(req.body);
-        }
-    });
-    
-    function executeStatement(p1) {
-        request = new Request("dbo.get_todo", (err) => {
-            if (err) {
-                context.log(err);
-            }
-            connection.close();
-            context.res = {
-                body: result
-            }
-            context.done();
-        });
-
-        request.on('row', columns => {
-            columns.forEach(column => {
-                result += column.value;                
-            });
-        });
-
-        connection.execSql(request);
-    }   
+    executeSQL(context, "put", payload)
 }
 
-module.exports = getTodo;
-
+module.exports = todoUpdate;
 
